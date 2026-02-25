18cbb16425822928161315e3e7914d9eb81c0cdd
fayazpatel455-design <fayazpatel455@gmail.com>
2026-02-25 23:26:14 +0530

    Fixed profile route handler export

--- backend/Routes/Profile.js
+++ backend/Routes/Profile.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const profile=require("../Controllers/Profile")
+const {profile}=require("../Controllers/Profile")
 
 router.get("/", profile);
 module.exports = router;
